d5ed0dfde14a8598baffc5d5171b90c94103df75
lailawaleed <147208546+lailawaleed@users.noreply.github.com>
2025-03-24 13:26:11 +0200

    Create 3169. Count Days Without Meetings.cpp

---
+++ LeetCode/Medium/3169. Count Days Without Meetings.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int countDays(int days, vector<vector<int>>& meetings) {
+        sort(meetings.begin(), meetings.end());
+        int freeDays = 0;
+        
+        int firstMeetingStart = meetings[0][0];
+        freeDays += firstMeetingStart - 1;
+
+        int lastEndTime = meetings[0][1]; 
+        
+        for (int i = 1; i < meetings.size(); i++) 
+        {
+            int currentStart = meetings[i][0];
+            int currentEnd = meetings[i][1];
+            
+            if (currentStart <= lastEndTime) 
+            {
+                lastEndTime = max(lastEndTime, currentEnd);
+            }
+            else 
+            {
+                freeDays += currentStart - lastEndTime - 1;
+                lastEndTime = currentEnd;
+            }
+        }       
+        freeDays += days - lastEndTime;
+        
+        return freeDays;
+    }
+};
